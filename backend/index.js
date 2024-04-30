e7a339719fa05ce8ae0b4d03c699d9fb4ad94157
Sameer <kontactsun@gmail.com>
2024-04-30 12:32:39 +0530

    Changing of backend to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
-import mongoose from "mongoose"
-import {DB_NAME from) "./constants"
 const express = require('express')
+const dotenv = require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
 const cors = require('cors')
@@ -12,122 +11,156 @@ app.get('/',(req,res) => {
     res.send("Hello World!")
 })
 
-// app.listen(port, () => {
-//     console.log(`Example app listening on port ${port}`)
-// })
+app.listen(port, (err) => {
+    if(err){
+        console.log(err)
+    }
+    else{
+    console.log(`Example app listening on port ${port}`)
+    }
+})
 
-k0oePjUKOgKwLXRk
-// const express = require("express")
-// var mysql = require("mysql")
-// var app = express()
-// const port = process.env.PORT || 5000
-// app.use(express.json())
+//mongoDB Configuration
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const mongo_username = process.env.MONGO_USERNAME;
+const mongo_password = process.env.MONGO_PASSWORD;
+const uri = `mongodb+srv://ebook-store:${mongo_password}@cluster0.3tcvnlj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
-// const con = mysql.createConnection({
-//     host:'localhost',
-//     user:'root',
-//     password:'',
-//     database:'ebook'
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
 
-// })
+    //Creating a collection of documents
+    const bookCollections = client.db("BookInventory").collection("books");
 
-// con.connect((err)=>{
-//     if(err)
-//     {
-//         console.log(err)
-//     }else{
-//         console.log(`Connection successfull`)
-//     }
-// })
+    //inserting a book to the db: post method
 
-// // app.post('/upload-book', async(req,res)=>{
-// //     const id = req.body.id;
-// //     const name = req.body.name;
-// //     const rollnumber= req.body.rollnumber;
-
-// //     con.query('insert into book values(?,?,?)',[id,name,rollnumber],(err,result)=>{
-// //         if(err)
-// //         {
-// //             console.log(err)
-// //         }
-// //         else{
-// //             console.log(`Successfully created customer with id:${id}`)
-// //         } 
-// //     })
-// // })
-
-// app.post('/upload-book', async(req,res)=>{
-//     var data = {
-//         id: id,
-//         authorName: req.body.authorName,
-//         category: req.body.category,
-//         description: req.body.description,
-//         title: req.body.title,
-//         image: req.file.imagename,
-//         bookPDF: req.file.filename,
-//     }
-//     con.query('insert into book values ? ',[data],(err,result)=>{
-//         if(err)
-//         {
-//             console.log(err)
-//         }
-//         else{
-//              console.log(`Successfully created book with id:${id}`)
-//             } 
-//         })
-//     })
+    app.post('/upload-book', async(req,res)=>{
+      const data = req.body;
+      const result = await bookCollections.insertOne(data);
+      res.send(result)
+    })
+    
+    
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 
 
-// //below code is to fetch data by particular ID
 
-// app.get('/fetchbyid/:id', async(req,res)=>{
-//     const fetchid = req.params.id;
-//     con.query('select * from db where id = ?',fetchid,(err,result)=>{
-//         if(err)
-//         {
-//             console.log(err)
-//         }
-//         else{
-//             if(result.length==0)
-//             {
-//                 console.log("id not present")
-//             }
-//             else{
-//                 var value=JSON.parse(JSON.stringify(result))
-//                 res.send(result)
-//                 //for converting to JSON format
-//                 //console.log(JSON.parse(JSON.stringify(result)))
-//             }
+/*MYSQL CONFIGURATION
+
+const con = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'ebook'
+
+})
+
+con.connect((err)=>{
+    if(err)
+    {
+        console.log(err)
+    }else{
+        console.log(`Connection successfull`)
+    }
+})
+
+app.post('/upload-book', async(req,res)=>{
+    const id = req.body.id;
+    const name = req.body.name;
+    const rollnumber= req.body.rollnumber;
+
+    con.query('insert into book values(?,?,?)',[id,name,rollnumber],(err,result)=>{
+        if(err)
+        {
+            console.log(err)
+        }
+        else{
+            console.log(`Successfully created customer with id:${id}`)
+        } 
+    })
+})
+
+app.post('/upload-book', async(req,res)=>{
+    var data = {
+        id: id,
+        authorName: req.body.authorName,
+        category: req.body.category,
+        description: req.body.description,
+        title: req.body.title,
+        image: req.file.imagename,
+        bookPDF: req.file.filename,
+    }
+    con.query('insert into book values ? ',[data],(err,result)=>{
+        if(err)
+        {
+            console.log(err)
+        }
+        else{
+             console.log(`Successfully created book with id:${id}`)
+            } 
+        })
+    })
+
+//below code is to fetch data by particular ID
+
+app.get('/fetchbyid/:id', async(req,res)=>{
+    const fetchid = req.params.id;
+    con.query('select * from db where id = ?',fetchid,(err,result)=>{
+        if(err)
+        {
+            console.log(err)
+        }
+        else{
+            if(result.length==0)
+            {
+                console.log("id not present")
+            }
+            else{
+                var value=JSON.parse(JSON.stringify(result))
+                res.send(result)
+                //for converting to JSON format
+                //console.log(JSON.parse(JSON.stringify(result)))
+            }
             
-//         } 
-//     })  
-// })
+        } 
+    })  
+})
 
-// //The below code is to fetch all the data from database
+//The below code is to fetch all the data from database
 
-// app.get("/fetchall",async(req,res)=>{
-//     con.query("select * from db", function(err,result,fields){
-//         if(err)
-//         {
-//             console.log(err)
-//         }
-//         else
-//         {
-//             res.send(result)
-//         }
+app.get("/fetchall",async(req,res)=>{
+    con.query("select * from db", function(err,result,fields){
+        if(err)
+        {
+            console.log(err)
+        }
+        else
+        {
+            res.send(result)
+        }
 
-//     })
-// })
-
-// app.listen(port, (err) => {
-//     if(err){
-//         console.log(err)
-//     }
-//     else{
-//     console.log(`Example app listening on port ${port}`)
-//     }
-// })
+    })
+})
+*/
 
 
 
